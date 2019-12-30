3d491692d5ce30a794a36c84ffc7b0f02ec7fe77
superyaz <yacamo04@hotmail.com>
2019-12-30 09:59:10 -0500

    Se crea el los archivos de configuración para la base de datos, esquema, rutas y app principal, se deja corriendo el servidor por el puerto 3000

--- app.js
+++ app.js
@@ -3,15 +3,45 @@ const express = require('express');
 const consolidate = require('consolidate');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const routes = require('./routes/routes');
 
 const app = express();
 
 
+app.use(bodyParser.urlencoded({
+    extended: true,
+}));
+
+app.use(bodyParser.json({
+    limit: '5mb'
+}));
+
+app.set('views', 'views');
+app.use(express.static('./public'));
+
+app.set('view engine', 'html');
+app.engine('html', consolidate.handlebars);
+
+//Connect to Database
+
+const db = 'mongodb://localhost:27017/uberForX';
+mongoose.connect(db).then(value => {
+    console.log(value.model)
+}).catch(error => {
+    console.log(error);
+});
+
+
+//Requiero la ruta
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
+const server = http.server(app);
 
-app.listen(3000, (req, res) => {
+const portNumber = 3000;
+
+
+server.listen(portNumber, (req, res) => {
     console.log('Server on port 3000');
-})
+});
